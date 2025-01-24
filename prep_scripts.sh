dca7f3ba1a0d070c37596208c739968869a78f99
mbido <ma.bidault39@gmail.com>
2025-01-24 09:45:25 +0100

    Mise à jour du .gitignore et amélioration du script prep_scripts.sh pour traiter les fichiers .sage dans les sous-dossiers.

--- prep_scripts.sh
+++ prep_scripts.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cd src
-for file in *.sage; do
+for file in */*.sage; do
+  sage "$file" > /dev/null
+  mv "${file}.py" "${file%.sage}.py"
+done
+
+# a second time for any interdependencies
+for file in */*.sage; do
   sage "$file" > /dev/null
   mv "${file}.py" "${file%.sage}.py"
 done
